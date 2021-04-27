b3a23b58dc151643204491caff320c2137308845
Boris Aelen <boris@borisaelen.nl>
2021-04-27 12:21:31 +0200

    allignment

--- iks.tf
+++ iks.tf
@@ -1,16 +1,16 @@
 module "iks" {
-  source  = "terraform-cisco-modules/iks/intersight"
-  version = "0.9.20"
+  source              = "terraform-cisco-modules/iks/intersight"
+  version             = "0.9.20"
   
   # Infra Config Policy Information
-  cluster_name = "iks-tenant-a"
-  # cluster_action = "Deploy"
-  vc_target_name   = "10.61.124.9"
-  vc_portgroup     = [ "vlan99-99" ]
-  vc_datastore     = "HX-NVME" 
-  vc_cluster       = "HX-NVME"
-  vc_resource_pool = ""
-  vc_password      = var.vc_password
+  cluster_name        = "iks-tenant-a"
+  # cluster_action    = "Deploy"
+  vc_target_name      = "10.61.124.9"
+  vc_portgroup        = [ "vlan99-99" ]
+  vc_datastore        = "HX-NVME" 
+  vc_cluster          = "HX-NVME"
+  vc_resource_pool    = ""
+  vc_password         = var.vc_password
 
   # IP Pool Information
   ip_starting_address = "10.61.125.150"
@@ -25,11 +25,11 @@ module "iks" {
   timezone            = "Europe/Amsterdam"
 
   # Cluster information
-  ssh_user     = "iksadmin"
-  ssh_key      = var.ssh_key
-  worker_size  = "small"
-  worker_count = 4
-  master_count = 1
+  ssh_user            = "iksadmin"
+  ssh_key             = var.ssh_key
+  worker_size         = "small"
+  worker_count        = 4
+  master_count        = 1
   # Organization
-  organization = var.organization
+  organization        = var.organization
 }
