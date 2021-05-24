29196fb82b4dfd63c949b82d32450da01d8a7766
Boris Aelen <boris@borisaelen.nl>
2021-05-24 15:45:32 +0200

    new version

--- iks.tf
+++ iks.tf
@@ -1,7 +1,7 @@
 module "iks-amslab" {
   #source              = "../../terraform-intersight-iks"
   source              = "app.terraform.io/Cisco-IST-TigerTeam/iks-amslab/intersight"
-  version             = "0.9.22"
+  version             = "0.9.23"
   
   # Infra Config Policy Information
   cluster_name        = "cluster-b1"
