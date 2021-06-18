f8712951e0bba3d02207eca8e82615690598ae22
John Colcombe <jcolcomb@cisco.com>
2021-06-18 15:28:49 +0100

    Update iks.tf

--- iks.tf
+++ iks.tf
@@ -2,33 +2,33 @@ module "iks-amslab" {
   #source              = "../../terraform-intersight-iks"
   source              = "app.terraform.io/Cisco-IST-TigerTeam/iks-amslab/intersight"
   version             = "0.9.23"
-  
+
   # Infra Config Policy Information
-  cluster_name        = "cluster-b1"
+  cluster_name        = "RTP-Sandpit01"
   cluster_action      = "Deploy"
-  vc_target_name      = "vcenter-amslab.cisco.com"
-  vc_portgroup        = [ "vm-network-99" ]
-  vc_datastore        = "HX-ACI" 
-  vc_cluster          = "HX-ACI"
-  vc_resource_pool    = "tenant-b"
+  vc_target_name      = "vc2.dcv.svpod"
+  vc_portgroup        = [ "sandpit|IST|demo" ]
+  vc_datastore        = "sandpit-ds1"
+  vc_cluster          = "RTP-Sandpit"
+  vc_resource_pool    = "IST-VMs"
   vc_password         = var.vc_password
 
   # IP Pool Information
-  ip_starting_address = "10.61.125.210"
+  ip_starting_address = "100.64.2.140"
   ip_pool_size        = "10"
   ip_netmask          = "255.255.255.0"
-  ip_gateway          = "10.61.125.1"
-  ip_primary_dns      = "10.61.124.15"
-  ip_secondary_dns    = "10.61.124.16"
-  ip_primary_ntp      = "ntp.esl.cisco.com"
+  ip_gateway          = "100.64.2.1"
+  ip_primary_dns      = "198.19.254.82"
+  ip_secondary_dns    = "198.19.255.137"
+  ip_primary_ntp      = "198.19.255.137"
 
   # Network Configuration Settings
-  domain_name         = "amslab.cisco.com"
-  timezone            = "Europe/Amsterdam"
-  http_proxy          = "proxy.esl.cisco.com"
-  https_proxy         = "proxy.esl.cisco.com"
-  http_proxy_port     = 80
-  https_proxy_port    = 80
+  domain_name         = "dcv.svpod"
+  timezone            = "Europe/London"
+  #http_proxy          = "proxy.esl.cisco.com"
+  #https_proxy         = "proxy.esl.cisco.com"
+  #http_proxy_port     = 80
+  #https_proxy_port    = 80
 
   # Cluster information
   ssh_user            = "iksadmin"
@@ -39,5 +39,3 @@ module "iks-amslab" {
   # Organization
   organization        = var.organization
 }
-
-
